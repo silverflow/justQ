1d4aaed85bbb6adde30339111908d0ab108c25c5
metafor <silveryflow@naver.com>
2020-02-06 22:19:18 +0900

    minor fixed(사용 안하는 모듈제거, 주석 추가)

--- insertDB.py
+++ insertDB.py
@@ -1,16 +1,18 @@
 from sqlalchemy import Column, String, Integer, Float, Date
-from sqlalchemy.dialects.mysql import BIGINT
 from sqlalchemy.ext.declarative import declarative_base
 import pymysql
 import pandas as pd
 from sqlalchemy import create_engine
 
 
+# 제 개인 연습용 AWS 입니다. 테스트 환경에 맞게 밑에 설정을 바꿔주세요
+# 아이디:비밀번호@호스트명:포트/DB명
 engine = create_engine(
     "mysql+pymysql://root:8884@ec2-13-125-246-153.ap-northeast-2.compute.amazonaws.com:3306/my_database", encoding='utf-8-sig')
 
-
 Base = declarative_base()
+
+# ORM Class
 
 
 class salesOrdersRaw(Base):
@@ -26,8 +28,10 @@ class salesOrdersRaw(Base):
     Total = Column(Float())
 
 
+# DB에 메타데이터 생성
 metadata = Base.metadata
 metadata.create_all(engine)
 
+# DB에 데이터 넣기
 df = pd.read_excel('SampleData.xlsx', sheet_name='SalesOrders', header=0)
 df.to_sql(name='sales_order', con=engine, if_exists='append', index=False)
